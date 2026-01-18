49648b5be2a26e95ea6ff33dee3276a640744fb7
BHAGAVATHIRAJA26 <bhagavathiraja.s26@gmail.com>
2026-01-18 20:38:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "client",
   plugins: [react()],
-
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3500",
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
-
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     emptyOutDir: true
   }
 });
