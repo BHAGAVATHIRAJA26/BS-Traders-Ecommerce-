c874b656435765e626d39c9017cfe9a3e3e59f1b
BHAGAVATHIRAJA26 <bhagavathiraja.s26@gmail.com>
2026-01-18 20:55:11 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx' 
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import Reg from './Newreg.jsx'
@@ -49,3 +48,4 @@ createRoot(document.getElementById('root')).render(
 
   </StrictMode>,
 )
+
